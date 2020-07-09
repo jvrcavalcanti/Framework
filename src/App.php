0e3b26b17cf8bd5f990efff6ab931649bc0c2adf
Jvrcavalcanti <jonnyvictor01@gmail.com>
2020-07-09 01:22:53 -0300

    App Exceptions

--- src/App.php
+++ src/App.php
@@ -2,9 +2,20 @@
 
 namespace Pendragon\Framework;
 
+use Accolon\Route\Response;
 use Accolon\Route\Router;
+use Pendragon\Framework\Exceptions\ValidateFailException;
 
 class App extends Router
 {
-    //
+    public function run()
+    {
+        try {
+            parent::run();
+        } catch (ValidateFailException $e) {
+            return (new Response)->json([
+                "message" => $e->getMessage()
+            ], 400);
+        }
+    }
 }
