9a27e2671a3dd2d1f03d8f3c15f6d4835e17a59b
jvrcavalcanti <jonnyvictor01@gmail.com>
2020-08-09 03:37:36 -0300

    Fiz merda

---
+++ src/Testing/TestService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pendragon\Framework\Testing;
+
+use Accolon\Request\Client;
+use Accolon\Request\Enums\ContentType;
+use PHPUnit\Framework\TestCase;
+
+class TestService extends TestCase
+{
+    private Client $client;
+
+    public function setUp(): void
+    {
+        $this->client = new Client([
+            'baseUrl' => env('APP_HOST') ?? "",
+            'contentType' => ContentType::JSON
+        ]);
+    }
+}
