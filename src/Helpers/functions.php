f023b9fdc76b47a96334725d12d9c8966131e1a5
jvrcavalcanti <jonnyvictor01@gmail.com>
2021-01-05 01:02:38 -0300

    eh

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+use Pendragon\Framework\App;
 use Symfony\Component\Dotenv\Dotenv;
 
 function session($param = null)
@@ -24,6 +25,25 @@ function env(string $attr, $default = "")
 function migrations(): string
 {
     return APP_ROOT . 'migrations';
+}
+
+function app(): App
+{
+    if (!isset($GLOBALS['app'])) {
+        throw new \Exception("Not exists app in global scope");
+    }
+
+    return $GLOBALS['app'];
+}
+
+function container($id = null)
+{
+    return is_null($id) ? app()->getContainer() : container()->get($id);
+}
+
+function resolve(string $class)
+{
+    return container()->make($class);
 }
 
 /* Util */
